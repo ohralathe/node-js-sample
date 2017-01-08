98f43eb8a2ee2b74952e06c4a49dd84af4543019
ohralathe <21206531hOAN>
2017-01-08 11:53:40 +0700

    add version file

---
+++ src/Test.groovy
@@ -0,0 +1,11 @@
+/**
+ * Created by hoanvo on 1/8/2017.
+ */
+class Test implements Serializable {
+    Test() {
+    }
+
+    void printST() {
+        println 'ccccccccccccccccccccccccccccc'
+    }
+}
